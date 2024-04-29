320a46bea7280d32036044da0993e4dd1be0ce7f
HarshKorde5 <141399156+HarshKorde5@users.noreply.github.com>
2024-04-29 17:38:35 +0530

    apr29

---
+++ Function1.py
@@ -0,0 +1,12 @@
+
+#Defination of function
+def Fun():
+    print("Inside Fun")
+
+#Function call
+Fun()
+
+def Gun():
+    pass
+
+Gun()
---
+++ Test.py
@@ -0,0 +1,22 @@
+
+print("Demo Python Program")
+
+No = 11
+print(id(No))
+
+Y = 11
+print(id(Y))
+
+
+Y = 38.23
+print(id(No))
+print(id(Y))
+
+Z = "11"
+print(id(Z))
+
+A = 11
+B = 11.90
+C = "Hello"
+D = True
+E = 9+2j
