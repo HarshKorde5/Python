04d3dbbbdcc2966b3fa767197b75dac9fbc47471
HarshKorde5 <harshkorde05@gmail.com>
2024-05-12 11:57:46 +0530

    12may

---
+++ openFile.py
@@ -0,0 +1,15 @@
+import os
+
+def main():
+    print("Enter the name of file that you want to open : ")
+    Fname = input()
+
+    if os.path.exists(Fname):
+        fobj = open(Fname,"r")
+        print("File is successfully opened")
+        print(fobj)
+    else:
+        print("Unable to open file as file is not present in current directory")
+
+if __name__ == "__main__":
+    main()
---
+++ readFileX.py
@@ -0,0 +1,21 @@
+import os
+
+def main():
+    print("Enter the name of file that you want to read : ")
+    Fname = input()
+
+    if os.path.exists(Fname):
+        fobj = open(Fname,"r")
+        print("File is successfully opened in appened mode")
+
+        Data = fobj.read(10)
+
+        print(Data)
+        
+        fobj.close()
+        
+    else:
+        print("Unable to open file as file is not present in current directory")
+
+if __name__ == "__main__":
+    main()
